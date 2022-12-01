b728e61b06c48e2e79b03b4208689abad01d1e82
villa <volinetsilia@gmail.com>
2022-12-01 15:42:29 +0200

    First commit

---
+++ game.py
@@ -0,0 +1,172 @@
+import pygame, sys
+from socket import socket, AF_INET, SOCK_STREAM
+import json
+from time import sleep
+from signal import signal, SIGINT
+import select
+# from re import findall
+
+
+WIDTH = 1000
+HEIGHT = 700
+WINDOW_NAME = 'The First Player'
+held_key = []
+screen = None
+HOST = 'localhost'
+PORT = 6787
+stop_program = False
+start_x = WIDTH//2
+start_y = HEIGHT//2
+diameter = 50
+player_color = (0, 0, 0)
+player_speed = 1
+id_player = 'The first player'
+cd_send = True
+
+
+class Player:
+
+    def __init__(self, x, y, d, col, screen, speed, player_id):
+        self.x = x
+        self.y = y
+        self.d = d
+        self.col = col
+        self.screen = screen
+        self.speed = speed
+        self.id_player = player_id
+
+    def draw(self):
+        pygame.draw.circle(self.screen, self.col, (self.x, self.y), self.d)
+
+
+def connect_server():
+    global stop_program
+    try:
+        client = socket(AF_INET, SOCK_STREAM)
+        client.connect((HOST, PORT))
+        client.setblocking(False)
+        return client
+    except ConnectionRefusedError:
+        print('You were not connected to server.')
+        stop_program = True
+        return False
+
+
+class MainController:
+
+    def __init__(self, client):
+        global screen
+        self.player = Player(start_x, start_y, diameter, player_color, screen, player_speed, id_player)
+        self.client = client
+        self.other_players = []
+
+    def update(self):
+        self.move_player()
+        self.draw_objects()
+        self.data_operations()
+
+    def draw_objects(self):
+        self.player.draw()
+        for player in self.other_players:
+            player.draw()
+
+    def move_player(self):
+        global held_key
+        for item in range(len(held_key)):
+            if held_key[item] == 'UP':
+                self.player.y -= self.player.speed
+            if held_key[item] == 'DOWN':
+                self.player.y += self.player.speed
+            if held_key[item] == 'LEFT':
+                self.player.x -= self.player.speed
+            if held_key[item] == 'RIGHT':
+                self.player.x += self.player.speed
+
+    def append_player(self, data):
+        self.other_players.append(Player(data['x'], data['y'], diameter, player_color,
+                                         screen, player_speed, data['player_id']))
+
+    def give_data_to_player(self, data):
+        global cd_send
+        cd_send = True
+        for elem in data[:]:
+            for player in self.other_players:
+                if player.id_player in elem['player_id']:
+                    player.x = elem['x']
+                    player.y = elem['y']
+                    data.remove(elem)
+                    break
+        for d in data:
+            self.append_player(d)
+
+    def send(self, data):
+        global cd_send
+        if cd_send:
+            cd_send = False
+            self.client.send(data.encode())
+
+    def accept_data(self):
+        info_player = ''
+        while True:
+            response = self.client.recv(1).decode()
+            if response == '\n':
+                return json.loads(info_player)
+            info_player += response
+
+    def data_operations(self):
+        global stop_program
+        try:
+            socket_ready_to_read, socket_ready_to_write, _ = select.select([self.client], [self.client], [], 0)
+            if self.client in socket_ready_to_write:
+                d = {'player_id': self.player.id_player, 'x': self.player.x, 'y': self.player.y}
+                self.send(json.dumps(d) + '\n')
+            if self.client in socket_ready_to_read:
+                self.give_data_to_player(self.accept_data())
+        except ConnectionAbortedError:
+            self.client.close()
+            stop_program = True
+
+
+def main():
+    global held_key, screen, stop_program
+    screen = pygame.display.set_mode((WIDTH, HEIGHT))
+    pygame.display.set_caption(WINDOW_NAME)
+    result = connect_server()
+    if result is not False:
+        main_controller = MainController(result)
+        while True:
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    pygame.quit()
+                    sys.exit()
+                elif event.type == pygame.KEYDOWN:
+                    if event.key == pygame.K_w:
+                        held_key.append('UP')
+                    if event.key == pygame.K_s:
+                        held_key.append('DOWN')
+                    if event.key == pygame.K_a:
+                        held_key.append('LEFT')
+                    if event.key == pygame.K_d:
+                        held_key.append('RIGHT')
+                elif event.type == pygame.KEYUP:
+                    held_key.clear()
+
+            screen.fill((255, 255, 255))
+            main_controller.update()
+            pygame.display.update()
+
+            if stop_program:
+                break
+
+        main_controller.client.close()
+
+
+def signal_handler(_, __):
+    global stop_program
+    print('Client closing...')
+    stop_program = True
+
+
+if __name__ == '__main__':
+    signal(SIGINT, signal_handler)
+    main()
---
+++ server.py
@@ -0,0 +1,103 @@
+import select
+from socket import socket, AF_INET, SOCK_STREAM
+import json
+from signal import signal, SIGINT
+from time import sleep
+
+HOST = ''
+PORT = 6787
+stop = False
+
+
+class Player:
+    client = None
+    client_ip = None
+    data = None
+
+
+players = []
+
+
+def read(sockets, host):
+    global players
+    for s in sockets:
+        if s is host:
+            player = Player()
+            player.client, player.client_ip = s.accept()
+            players.append(player)
+            print('Client connected ' + player.client_ip[0])
+        else:
+            for player in players[:]:
+                if s is player.client:
+                    recv_string = ''
+                    while True:
+                        recv_byte = player.client.recv(1).decode()
+                        if len(recv_byte) == 0:
+                            player.client.close()
+                            print('Client disconnected ' + player.client_ip[0])
+                            players.remove(player)
+                            break
+                        elif recv_byte == '\n':
+                            player_data = json.loads(recv_string)
+                            player.data = player_data
+                            break
+                        recv_string += recv_byte
+
+
+def write(sockets):
+    global players
+    for s in sockets:
+        senders = players[:]
+        data_senders = []
+        recipient = None
+        for player in players:
+            if player.client is s:
+                recipient = player
+                senders.remove(recipient)
+                break
+        for sender in senders:
+            if sender.data is not None:
+                data = sender.data
+                data_senders.append(data)
+        if len(data_senders) != 0:
+            recipient.client.send((json.dumps(data_senders) + '\n').encode())
+            data_senders.clear()
+
+
+def update_data():
+    global players
+    for player in players:
+        if player.data is not None:
+            player.data = None
+
+
+def main():
+    global players, stop
+
+    max_player = 2
+
+    server = socket(AF_INET, SOCK_STREAM)
+    server.bind((HOST, PORT))
+    server.listen(max_player)
+    while True:
+        clients = [player.client for player in players]
+        sockets_reading, sockets_writing, _ = select.select([server] + clients, clients, [], 1)
+        read(sockets_reading, server)
+        write(sockets_writing)
+        update_data()
+
+        if stop:
+            break
+
+    server.close()
+
+
+def signal_handler(_, __):
+    global stop
+    print('Server closing...')
+    stop = True
+
+
+if __name__ == '__main__':
+    signal(SIGINT, signal_handler)
+    main()
